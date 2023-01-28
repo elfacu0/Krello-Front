103def07224d8c58068fca568659d98ba6e26b4f
“elfacu0” <mahat.mejor@gmail.com>
2023-01-28 10:42:56 -0600

    Change submit button text to login

--- src/lib/LoginForm.svelte
+++ src/lib/LoginForm.svelte
@@ -8,5 +8,5 @@
 <div class="form-control card w-full bg-neutral shadow-xl p-8 flex justify-items-center justify-center">
   <Input label="Username" placeholder="Place Username" value={username} />
   <PasswordInput label="Password" placeholder="Place Password" value={password} />
-  <input type="submit" value="Submit" class="btn btn-secondary mt-8" />
+  <input type="submit" value="Login" class="btn btn-secondary mt-8" />
 </div>
