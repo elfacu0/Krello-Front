897dffeda335ec0f1c2ebdeaea20b54667306403
“elfacu0” <mahat.mejor@gmail.com>
2023-01-30 12:35:18 -0600

    Create new input component

---
+++ src/lib/EditInput.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let value: string;
+    export let label: string;
+    export let placeholder: string;
+    export let editedContent: string;
+
+    $: editedContent = value;
+</script>  
+     
+<div class="form-control">
+    <label class="label">
+        <span class="label-text">{label}</span>
+    </label>
+    <input
+        type="text"
+        placeholder="{placeholder}"
+        class="input input-bordered input-accent max-w-s"
+        bind:value={value}
+    />
+</div>
