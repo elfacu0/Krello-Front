a8482c076904e302b23f1f7154556d44cb6218bb
“elfacu0” <mahat.mejor@gmail.com>
2023-01-30 09:34:51 -0600

    Add settings page

---
+++ src/lib/CreateCollection.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    const token = localStorage.getItem("token");
+    let error: string;
+    let success: string;
+    let email: string;
+    let password: string;
+
+    if (token == null) {
+        window.location.replace("/");
+    }
+
+    $: handleSubmit = async () => {
+        error = "";
+        success = "";
+        try {
+            const response = await fetch(
+                "http://localhost:3000/collections/export",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const data = await response.json();
+            if(data.statusCode === 201){
+                success = "Collection created" + "ID = " + data.id;
+            }else{
+                error = data.message;
+            }
+        } catch (err) {
+            error = err.message;
+        }
+    };
+</script>
+
+<button on:click={handleSubmit}>
+    <label for="my-modal-7" class="btn">CREATE COLLECTION</label>
+</button>
+<input type="checkbox" id="my-modal-7" class="modal-toggle" />
+<div class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box">
+        <h3 class="font-bold text-lg">Create Collection</h3>
+        {#if error}
+            <div class="alert alert-error shadow-lg">
+                <div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="stroke-current flex-shrink-0 h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        /></svg
+                    >
+                    <span>{error}</span>
+                </div>
+            </div>
+        {/if}
+
+        {#if success}
+            <div class="alert alert-success shadow-lg">
+                <div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="stroke-current flex-shrink-0 h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                        /></svg
+                    >
+                    <span>{success}</span>
+                </div>
+            </div>
+        {/if}
+        <div class="modal-action">
+            <label for="my-modal-7" class="btn">Cancel</label>
+        </div>
+    </div>
+</div>
---
+++ src/pages/Settings.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    import Input from "../lib/Input.svelte";
+    import PasswordInput from "../lib/PasswordInput.svelte";
+
+    const token = localStorage.getItem("token");
+    let error: string;
+    let success: string;
+    let email: string;
+    let password: string;
+
+    if (token == null) {
+        window.location.replace("/");
+    }
+
+    $: handleSubmit = async () => {
+        error = "";
+        success = "";
+        try {
+            const response = await fetch("http://localhost:3000/users/edit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await response.json();
+            success = "Changes saved successfully";
+        } catch (err) {
+            error = err.message;
+        }
+    };
+</script>
+
+{#if error}
+    <div class="alert alert-error shadow-lg">
+        <div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="stroke-current flex-shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                /></svg
+            >
+            <span>{error}</span>
+        </div>
+    </div>
+{/if}
+
+{#if success}
+    <div class="alert alert-success shadow-lg">
+        <div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="stroke-current flex-shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                /></svg
+            >
+            <span>{success}</span>
+        </div>
+    </div>
+{/if}
+
+<form
+    class="form-control card w-full bg-neutral shadow-xl p-8 flex
+justify-items-center justify-center"
+    on:submit|preventDefault={handleSubmit}
+    method="POST"
+>
+    <Input label="Email" placeholder="Email" bind:value={email} />
+    <PasswordInput
+        label="Password"
+        placeholder="Password"
+        bind:value={password}
+    />
+    <input type="submit" value="Submit" class="btn btn-secondary mt-8" />
+</form>
